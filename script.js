712b6b8643d1098987866d362ccaeac2d2de58dc
CRobock <crobock07@gmail.com>
2022-07-11 19:35:44 -0400

    added JS unsuccesfully

---
+++ script.js
@@ -0,0 +1,12 @@
+// Pseudo Code - Nav dropdown menu
+
+// Identify element we will need to listen too
+
+
+// Could not get menuIcon to display: block;
+const slideOutButton = document.getElementsByClassName("slideOutNav");
+const menuIcon = document.querySelector(".menuIcon");
+
+menuIcon.addEventListener("click", function(event) {
+    console.log("clicked");
+});
